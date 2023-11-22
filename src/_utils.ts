fb7db7450bc99fc8efbedd1fc248f0c895710881
Josh Kennedy <976307+YoCodingJosh@users.noreply.github.com>
2023-11-22 14:09:14 -0600

    some basic stuff

---
+++ src/_utils.ts
@@ -0,0 +1,6 @@
+import { Context, Env } from "hono"
+import { HonoBase } from "hono/hono-base"
+
+export type Bindings = {
+  MAL_CLIENT_ID: string,
+};
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,29 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
+import { poweredBy } from 'hono/powered-by';
+import { prettyJSON } from 'hono/pretty-json';
 
-const app = new Hono()
+import { Bindings } from './_utils';
 
-app.get('/', (c) => c.text('Yo!'))
-app.get('/health', (c) => c.text('OK'))
+const app = new Hono<{ Bindings: Bindings }>();
 
-export default app
+app.use('*', poweredBy());
+app.use('*', prettyJSON());
+app.notFound((c) => c.json({ message: 'Not Found', ok: false }, 404));
+
+app.get('/', (c) => c.text('Yo!'));
+
+app.get('/health', async (c) => {
+  console.log(c.env.MAL_CLIENT_ID);
+
+  const response = await fetch('https://api.myanimelist.net/v2/anime/21273',
+    { headers: { 'X-MAL-CLIENT-ID': c.env.MAL_CLIENT_ID } }
+  );
+
+  if (response.ok) {
+    return c.json({ ok: true, services: ['mal']});
+  } else {
+    return new Response('MyAnimeList is unavailable', { status: response.status });
+  }
+});
+
+export default app;
