bd15a035cdade218f9d36f7c1d20785729e59a79
Josh Kennedy <976307+YoCodingJosh@users.noreply.github.com>
2024-01-29 22:01:25 -0600

    Add studio watch time and some optimizations/clean up

--- src/stats/studios.ts
+++ src/stats/studios.ts
@@ -16,20 +16,39 @@ type StudioData = {
 
   // Total time spent watching anime from this studio
   watchTime: number;
+
+  // Average score of anime from this studio
+  averageScore?: number;
+
+  // Total score of anime from this studio
+  totalScore?: number; // this is only used when calculating the average score and is deleted afterwards
 }
 
 export async function getStudioStats(watchlist: WatchlistDataRequest): Promise<Record<number, StudioData>> {
-  return watchlist.data.filter((item) =>
-    item.list_status.num_episodes_watched > 0 &&
-    !['plan_to_watch'].includes(item.list_status.status)
-  ).reduce((acc: Record<number, StudioData>, item) => {
-    item.node.studios.forEach((studio) => {
-      if (!acc[studio.id]) {
-        acc[studio.id] = { id: studio.id, name: studio.name, anime: [], watchTime: 0 };
-      }
-      acc[studio.id].anime.push(item);
-      acc[studio.id].watchTime += item.list_status.num_episodes_watched * item.node.average_episode_duration;
-    });
+  const rawStudioData = watchlist.data.reduce((acc: Record<number, StudioData>, item) => {
+    const { list_status, node } = item;
+    const { num_episodes_watched, score } = list_status;
+    const { studios, average_episode_duration } = node;
+
+    if (num_episodes_watched > 0) {
+      studios.forEach((studio) => {
+        const { id, name } = studio;
+        if (!acc[id]) {
+          acc[id] = { id, name, anime: [], watchTime: 0, totalScore: 0 };
+        }
+        acc[id].anime.push(item);
+        acc[id].watchTime += num_episodes_watched * average_episode_duration;
+        acc[id].totalScore! += score;
+      });
+    }
     return acc;
   }, {});
+
+  Object.values(rawStudioData).forEach((studioData) => {
+    const { totalScore, anime } = studioData;
+    studioData.averageScore = totalScore! / anime.length;
+    delete studioData.totalScore;
+  });
+
+  return rawStudioData;
 }
